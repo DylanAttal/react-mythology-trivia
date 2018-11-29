6920c7d2713abaf83214f9ecf229e3757274dfd8
Dylan Attal <attal.dylan@gmail.com>
2018-11-29 16:16:42 -0500

    Adds basic styling

--- src/App.js
+++ src/App.js
@@ -37,81 +37,89 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <main>
         <header>
           <h1>Mythology Trivia!</h1>
         </header>
-        <div>
+        <div className="question-container">
           <h4>{this.state.game[0]['question']}</h4>
-          <p>{this.state.game[0]['incorrect_answers'][0]}</p>
-          <p>{this.state.game[0]['incorrect_answers'][1]}</p>
-          <p>{this.state.game[0]['incorrect_answers'][2]}</p>
-          <p>{this.state.game[0]['correct_answer']}</p>
+          <button className="incorrect-answer">
+            {this.state.game[0]['incorrect_answers'][0]}
+          </button>
+          <button className="incorrect-answer">
+            {this.state.game[0]['incorrect_answers'][1]}
+          </button>
+          <button className="incorrect-answer">
+            {this.state.game[0]['incorrect_answers'][2]}
+          </button>
+          <button className="correct-answer">
+            {this.state.game[0]['correct_answer']}
+          </button>
         </div>
-        <div>
+        <div className="question-container">
           <h4>{this.state.game[1]['question']}</h4>
-          <p>{this.state.game[1]['incorrect_answers'][0]}</p>
-          <p>{this.state.game[1]['incorrect_answers'][1]}</p>
-          <p>{this.state.game[1]['incorrect_answers'][2]}</p>
-          <p>{this.state.game[1]['correct_answer']}</p>
+          <button>{this.state.game[1]['incorrect_answers'][0]}</button>
+          <button>{this.state.game[1]['incorrect_answers'][1]}</button>
+          <button>{this.state.game[1]['incorrect_answers'][2]}</button>
+          <button>{this.state.game[1]['correct_answer']}</button>
         </div>
-        <div>
+        <div className="question-container">
           <h4>{this.state.game[2]['question']}</h4>
-          <p>{this.state.game[2]['incorrect_answers'][0]}</p>
-          <p>{this.state.game[2]['incorrect_answers'][1]}</p>
-          <p>{this.state.game[2]['incorrect_answers'][2]}</p>
-          <p>{this.state.game[2]['correct_answer']}</p>
+          <button>{this.state.game[2]['incorrect_answers'][0]}</button>
+          <button>{this.state.game[2]['incorrect_answers'][1]}</button>
+          <button>{this.state.game[2]['incorrect_answers'][2]}</button>
+          <button>{this.state.game[2]['correct_answer']}</button>
         </div>
-        <div>
+        <div className="question-container">
           <h4>{this.state.game[3]['question']}</h4>
-          <p>{this.state.game[3]['incorrect_answers'][0]}</p>
-          <p>{this.state.game[3]['incorrect_answers'][1]}</p>
-          <p>{this.state.game[3]['incorrect_answers'][2]}</p>
-          <p>{this.state.game[3]['correct_answer']}</p>
+          <button>{this.state.game[3]['incorrect_answers'][0]}</button>
+          <button>{this.state.game[3]['incorrect_answers'][1]}</button>
+          <button>{this.state.game[3]['incorrect_answers'][2]}</button>
+          <button>{this.state.game[3]['correct_answer']}</button>
         </div>
-        <div>
+        <div className="question-container">
           <h4>{this.state.game[4]['question']}</h4>
-          <p>{this.state.game[4]['incorrect_answers'][0]}</p>
-          <p>{this.state.game[4]['incorrect_answers'][1]}</p>
-          <p>{this.state.game[4]['incorrect_answers'][2]}</p>
-          <p>{this.state.game[4]['correct_answer']}</p>
+          <button>{this.state.game[4]['incorrect_answers'][0]}</button>
+          <button>{this.state.game[4]['incorrect_answers'][1]}</button>
+          <button>{this.state.game[4]['incorrect_answers'][2]}</button>
+          <button>{this.state.game[4]['correct_answer']}</button>
         </div>
-        <div>
+        <div className="question-container">
           <h4>{this.state.game[5]['question']}</h4>
-          <p>{this.state.game[5]['incorrect_answers'][0]}</p>
-          <p>{this.state.game[5]['incorrect_answers'][1]}</p>
-          <p>{this.state.game[5]['incorrect_answers'][2]}</p>
-          <p>{this.state.game[5]['correct_answer']}</p>
+          <button>{this.state.game[5]['incorrect_answers'][0]}</button>
+          <button>{this.state.game[5]['incorrect_answers'][1]}</button>
+          <button>{this.state.game[5]['incorrect_answers'][2]}</button>
+          <button>{this.state.game[5]['correct_answer']}</button>
         </div>
-        <div>
+        <div className="question-container">
           <h4>{this.state.game[6]['question']}</h4>
-          <p>{this.state.game[6]['incorrect_answers'][0]}</p>
-          <p>{this.state.game[6]['incorrect_answers'][1]}</p>
-          <p>{this.state.game[6]['incorrect_answers'][2]}</p>
-          <p>{this.state.game[6]['correct_answer']}</p>
+          <button>{this.state.game[6]['incorrect_answers'][0]}</button>
+          <button>{this.state.game[6]['incorrect_answers'][1]}</button>
+          <button>{this.state.game[6]['incorrect_answers'][2]}</button>
+          <button>{this.state.game[6]['correct_answer']}</button>
         </div>
-        <div>
+        <div className="question-container">
           <h4>{this.state.game[7]['question']}</h4>
-          <p>{this.state.game[7]['incorrect_answers'][0]}</p>
-          <p>{this.state.game[7]['incorrect_answers'][1]}</p>
-          <p>{this.state.game[7]['incorrect_answers'][2]}</p>
-          <p>{this.state.game[7]['correct_answer']}</p>
+          <button>{this.state.game[7]['incorrect_answers'][0]}</button>
+          <button>{this.state.game[7]['incorrect_answers'][1]}</button>
+          <button>{this.state.game[7]['incorrect_answers'][2]}</button>
+          <button>{this.state.game[7]['correct_answer']}</button>
         </div>
-        <div>
+        <div className="question-container">
           <h4>{this.state.game[8]['question']}</h4>
-          <p>{this.state.game[8]['incorrect_answers'][0]}</p>
-          <p>{this.state.game[8]['incorrect_answers'][1]}</p>
-          <p>{this.state.game[8]['incorrect_answers'][2]}</p>
-          <p>{this.state.game[8]['correct_answer']}</p>
+          <button>{this.state.game[8]['incorrect_answers'][0]}</button>
+          <button>{this.state.game[8]['incorrect_answers'][1]}</button>
+          <button>{this.state.game[8]['incorrect_answers'][2]}</button>
+          <button>{this.state.game[8]['correct_answer']}</button>
         </div>
-        <div>
+        <div className="question-container">
           <h4>{this.state.game[9]['question']}</h4>
-          <p>{this.state.game[9]['incorrect_answers'][0]}</p>
-          <p>{this.state.game[9]['incorrect_answers'][1]}</p>
-          <p>{this.state.game[9]['incorrect_answers'][2]}</p>
-          <p>{this.state.game[9]['correct_answer']}</p>
+          <button>{this.state.game[9]['incorrect_answers'][0]}</button>
+          <button>{this.state.game[9]['incorrect_answers'][1]}</button>
+          <button>{this.state.game[9]['incorrect_answers'][2]}</button>
+          <button>{this.state.game[9]['correct_answer']}</button>
         </div>
-      </div>
+      </main>
     )
   }
 }
